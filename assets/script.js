0ba9b639658101fe673d585e32bcf78817d5934a
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-17 13:53:42 +0000

    document is populated with a some content on searching a movie name

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,46 @@
-console.log("hello")
+let textInput = document.querySelector("#textInput");
+let submitBtn = document.querySelector("#submitBtn");
+
+
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    fetch(`http://www.omdbapi.com/?t=${textInput.value}&apikey=6aa91fd2`)
+    
+    .then(function (response) {
+
+        console.log(response.status);
+
+        return response.json();
+    })
+
+    .then(function(data){
+        console.log(data);
+
+        let actor = data.Actors
+        let actorSep = actor.split(",");
+        console.log(actorSep);
+
+        for(let i = 0; i < actorSep.length; i++) {
+            let actorCon = document.createElement("div");
+            actorCon.classList.add("actorCon");
+            actorCon.innerHTML = actorSep[i];
+            document.body.appendChild(actorCon);
+        }
+
+
+        let hero = document.createElement("img");
+        hero.setAttribute("src", data.Poster);
+        hero.classList.add("hero");
+        document.body.append(hero);
+
+  
+        let plot = document.body.append(data.Plot);
+      
+        
+
+
+
+    })
+
+})
